4c56571b9d650df9e9cab3b77369229a108b1f21
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2020-01-21 21:10:14 +0530

    added solution to day-2 problem of 30 days of code

---
+++ hackerrank/30-days-of-code/30-data-types/30-data-types.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+j = int(input())
+d2 = int(input())
+s2 = input()
+# Print the sum of both integer variables on a new line.
+print(i + j)
+print(d + d2)
+print(s + s2)
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+
