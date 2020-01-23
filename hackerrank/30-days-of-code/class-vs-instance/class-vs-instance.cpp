4d209fc6ba430a0f16706b7bd568ed50f297456e
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2020-01-23 18:22:52 +0530

    added class vs instance program in cpp

--- hackerrank/30-days-of-code/class-vs-instance/class-vs-instance.cpp
+++ hackerrank/30-days-of-code/class-vs-instance/class-vs-instance.cpp
@@ -1,6 +1,58 @@
 #include <iostream>
+#include <bits/stdc++.h>
 
-int main(int argc, char *argv[]) {
-    std::cout << "Hello world!!" << std::endl;
+using namespace std;
+
+class Person{
+    public:
+        int age;
+        Person(int initialAge);
+        void amIOld();
+        void yearPasses();
+    };
+
+    Person::Person(int initialAge){
+        age = initialAge;
+        if (age < 0) {
+            cout << "Age is not valid, setting age to 0." << endl;
+            age = 0;
+        }
+        // Add some more code to run some checks on initialAge
+    }
+
+    void Person::amIOld(){
+        if (age < 13) {
+            cout << "You are young." << endl;
+        } else if (age >= 13 && age < 18) {
+            cout << "You are a teenager." << endl;
+        } else {
+            cout << "You are old." << endl;
+        }
+        
+        // Do some computations in here and print out the correct statement to the console 
+    }
+
+    void Person::yearPasses(){
+        age++;
+        // Increment the age of the person in here
+
+    }
+
+int main(){
+    int t;
+	int age;
+    cin >> t;
+    for(int i=0; i < t; i++) {
+    	cin >> age;
+        Person p(age);
+        p.amIOld();
+        for(int j=0; j < 3; j++) {
+        	p.yearPasses(); 
+        }
+        p.amIOld();
+      
+		cout << '\n';
+    }
+
     return 0;
 }
