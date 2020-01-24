78e344a1d10c6d156fc1939b0e955d7dc13fa654
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2020-01-24 11:22:06 +0530

    added data types program in cpp

--- hackerrank/30-days-of-code/data-types/data-types.cpp
+++ hackerrank/30-days-of-code/data-types/data-types.cpp
@@ -17,14 +17,18 @@ int main() {
     string s2;
     // Read and save an integer, double, and String to your variables.
     cin >> j;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
     cin >> d2;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
     getline(cin >> ws, s2);
     // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
     
     // Print the sum of both integer variables on a new line.
     cout << i + j << endl;
+    
     // Print the sum of the double variables on a new line.
-    cout << d + d2 << endl;
+    cout << fixed << setprecision(1) << d + d2 << endl;
     // Concatenate and print the String variables on a new line
     // The 's' variable above should be printed first.
     cout << s << s2 << endl;
