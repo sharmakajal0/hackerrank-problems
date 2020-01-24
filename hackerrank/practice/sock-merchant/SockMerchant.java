2bb295f2334ea87efd1e33414f4cb2b1cb6c3adf
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2020-01-24 11:24:06 +0530

    added sock-merchant

---
+++ hackerrank/practice/sock-merchant/SockMerchant.java
@@ -0,0 +1,5 @@
+public class SockMerchant {
+  public static void main(String[] args) {
+    System.out.printf("SockMerchant java program\n");
+  }
+}
---
+++ hackerrank/practice/sock-merchant/sock-merchant.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+
+int main() {
+    std::cout << "sock merchant cpp program" << std::endl;
+    return 0;
+}
