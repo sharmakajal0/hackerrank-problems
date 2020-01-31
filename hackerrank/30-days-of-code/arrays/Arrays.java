7dc1e791a7aa35c8b4462e65bfce2323490921ae
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2020-01-31 10:16:27 +0530

    added arrays program in cpp and java

--- hackerrank/30-days-of-code/arrays/Arrays.java
+++ hackerrank/30-days-of-code/arrays/Arrays.java
@@ -1,5 +1,32 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class Arrays {
-  public static void main(String[] args) {
-    System.out.printf("Arrays java program\n");
-  }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i =n-1; i>=0; i--) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+            System.out.print(arr[i]);
+            System.out.print(" ");
+        }
+        System.out.println();
+
+        scanner.close();
+    }
 }
