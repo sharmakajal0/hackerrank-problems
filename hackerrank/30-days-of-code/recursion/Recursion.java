78af68cb22c56a18648d4695739abb362bea869b
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2020-01-31 10:17:13 +0530

    added recursion program in java and cpp

--- hackerrank/30-days-of-code/recursion/Recursion.java
+++ hackerrank/30-days-of-code/recursion/Recursion.java
@@ -1,5 +1,37 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class Recursion {
-  public static void main(String[] args) {
-    System.out.printf("Recursion java program\n");
-  }
+
+    // Complete the factorial function below.
+    static int factorial(int n) {
+        int value = 1;
+        for (int i = 2; i < n+1; i++) {
+            value = i * factorial(n-1);
+        }
+        return value;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = factorial(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
--- hackerrank/30-days-of-code/recursion/recursion.cpp
+++ hackerrank/30-days-of-code/recursion/recursion.cpp
@@ -1,6 +1,31 @@
-#include<iostream>
+#include <bits/stdc++.h>
 
-int main() {
-    std::cout << "recursion cpp program" << std::endl;
+using namespace std;
+
+// Complete the factorial function below.
+int factorial(int n) {
+    int value = 1;
+    for (int i = 2; i < n+1; i++)
+    {
+        value = i * factorial(n-1);
+    }
+
+    return value;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = factorial(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
     return 0;
 }
