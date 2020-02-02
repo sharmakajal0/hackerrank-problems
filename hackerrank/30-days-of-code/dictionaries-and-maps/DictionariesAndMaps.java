aed409f6442c513762c1b2bb993eb2f72d37818b
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2020-02-02 19:14:47 +0530

    added dictionaries and maps program in java with inputs attached

--- hackerrank/30-days-of-code/dictionaries-and-maps/DictionariesAndMaps.java
+++ hackerrank/30-days-of-code/dictionaries-and-maps/DictionariesAndMaps.java
@@ -1,5 +1,29 @@
+import java.util.*;
+import java.util.Dictionary;
+import java.util.Map;
+import java.util.Scanner;
+
 public class DictionariesAndMaps {
-  public static void main(String[] args) {
-    System.out.printf("DictionariesAndMaps java program\n");
-  }
+    public static void main(String[] args) {
+        Map<String, String> phoneBook = new HashMap<String, String>();       
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            String name = scanner.next();
+            String phone = scanner.next();
+            phoneBook.put(name, phone);
+        }
+        while(scanner.hasNext()){
+            String s = scanner.next();
+            if (phoneBook.containsKey(s)) {
+                System.out.printf("%s=%s", s, phoneBook.get(s));
+                System.out.println();
+            }
+            else {
+                System.out.println("Not found");
+            }
+        }
+        // System.out.println(phoneBook);
+        scanner.close();
+    }
 }
