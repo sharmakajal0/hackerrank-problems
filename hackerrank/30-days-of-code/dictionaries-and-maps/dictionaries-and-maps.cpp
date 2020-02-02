8359fc875cbca54f9f53c032f2c835ca4fcc7da8
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2020-02-02 14:33:34 +0530

    added dictionary and maps program in cpp

--- hackerrank/30-days-of-code/dictionaries-and-maps/dictionaries-and-maps.cpp
+++ hackerrank/30-days-of-code/dictionaries-and-maps/dictionaries-and-maps.cpp
@@ -1,6 +1,35 @@
-#include<iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <map>
+
+using namespace std;
 
 int main() {
-    std::cout << "dictionaries-and-maps cpp program" << std::endl;
+    map<string, string> phonebook;
+    int n;
+    cin >> n;
+    
+    for(int i = 0; i < n; i++){
+        string name, phone;
+        cin >> name >> phone;
+        phonebook[name] = phone;
+    }
+    
+    map<string, string>::iterator search;
+    string query;
+    while(cin >> query) {
+        search = phonebook.find(query);
+        
+        if ( search != phonebook.end() ){
+            cout << (*search).first << "=" << (*search).second << endl;
+        }
+        else {
+            cout << "Not found" << endl;
+        }
+    }
+    
     return 0;
 }
