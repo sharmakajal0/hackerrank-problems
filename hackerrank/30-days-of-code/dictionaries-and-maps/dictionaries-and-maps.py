b1847dec59cdfbae6f1c252a7590496ad2255b42
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2020-02-03 23:31:06 +0530

    modified dictionaries and maps in python

--- hackerrank/30-days-of-code/dictionaries-and-maps/dictionaries-and-maps.py
+++ hackerrank/30-days-of-code/dictionaries-and-maps/dictionaries-and-maps.py
@@ -2,18 +2,60 @@
 
 n = int(input())
 
-phonebook = {}
+class PhoneBook(object):
+    def __init__(self):
+        super().__init__()
+        self.phonebook = {}
+
+    def add_entry(self, name: str, phone_number: str) -> None:
+        """Adds entry to phone book for name and phone number.
+        Parameters
+        ----------
+        name : str
+            Name in phone book
+        phone_number : str
+            Phone Number in phone book
+        """
+        phonebook[name] = phone_number
+
+    def exists(self, name: str) -> bool:
+        """Check if entry with name exits in phonebook.
+        
+        Parameters
+        ----------
+        name : str
+            Name for which to check
+        
+        Returns
+        -------
+        bool
+            True if name exists in phone book otherwise False
+        """
+        return name in self.phonebook
+
+    def find(self, name: str) -> str:
+        """Searches for name in phone book and returns phone number against it.
+        Parameters
+        ----------
+        name : str
+            Name for which to find the phone number
+        """
+        return self.phonebook.get(name, None)
+
+
+phone_book = PhoneBook()
 
 for _ in range(n):
     name, number = input().split()
-    phonebook[name] = number
-
+    phone_book.add_entry(name, number)
 
 while(True):
     try:
         name = input()
-        if name in phonebook:
-            print("%s=%s" % (name, phonebook[name]))
+        phone_book.ex
+        if phone_book.exists(name):
+            phone_number = phone_book.find(name)
+            print("%s=%s" % (name, phone_number))
         else:
             print("Not Found")
     except EOFError:
