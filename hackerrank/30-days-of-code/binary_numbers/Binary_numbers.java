f139782405bfaae543355d616d4d3d551e0b5f35
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2020-02-03 09:33:44 +0530

    added binary numbers program in java

--- hackerrank/30-days-of-code/binary_numbers/Binary_numbers.java
+++ hackerrank/30-days-of-code/binary_numbers/Binary_numbers.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Binary_numbers {
-  public static void main(String[] args) {
-    System.out.printf("Binary_numbers java program\n");
-  }
+	public static void main(String[] args) {
+		int n;
+		Scanner scanner = new Scanner(System.in);
+		n = scanner.nextInt();
+		String str = Integer.toBinaryString(n);
+		str.toCharArray();
+		int counter = 0;
+		int j = 0;
+	
+		for(int i=0;i<=str.length()-1;i++) {
+			if (str.charAt(i)=='1') {
+				j++;
+      		}
+			else {
+        		j = 0;
+			}
+			if (j > counter) {
+        		counter = j;
+      		}
+    	}
+		System.out.println(counter);
+		scanner.close();
+  	}
 }
