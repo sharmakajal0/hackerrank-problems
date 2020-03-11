1cb7a63819ec634362729752d0cdc6e40e222bbe
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2020-03-11 22:18:37 +0530

    added data types program in java

--- hackerrank/challenges/data-types/DataTypes.java
+++ hackerrank/challenges/data-types/DataTypes.java
@@ -14,14 +14,14 @@ public class DataTypes {
             {
                 long x=sc.nextLong();
                 System.out.println(x+" can be fitted in:");
-                if(x>=-128 && x<=127){
+                if(x >= -128 && x <= 127){
                     System.out.println("* byte");
                 }
                 //Complete the code
-                if(x >= -32768 && x <= 32767){
+                if(x >= -(Math.pow(2, 15)) && x <= (Math.pow(2, 15) - 1)){
                     System.out.println("* short");
                 }
-                if(x >= -2147483648 && x <= 2147483647){
+                if(x >= -(Math.pow(2, 31)) && x <= (Math.pow(2, 31) - 1)){
                     System.out.println("* int");
                 }
                 if(x >= -(Math.pow(2, 63)) && x <= (Math.pow(2, 63) - 1)){
