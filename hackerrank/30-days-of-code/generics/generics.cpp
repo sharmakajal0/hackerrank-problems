3dd9ea7afe1f5101f78e114be20823c980970457
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2020-03-11 22:16:56 +0530

    added generics program in cpp

--- hackerrank/30-days-of-code/generics/generics.cpp
+++ hackerrank/30-days-of-code/generics/generics.cpp
@@ -11,35 +11,39 @@ using namespace std;
 **/
 
 // Write your code here
-void printArray(int * generic_vector){
-    for (int i = 0; i < sizeof(generic_vector); i++)
-    {
-        cout << generic_vector[i] << " ";
-    }
-    cout << endl;
+template<typename T> 
+	void printArray(vector<T> array) {
+	// 	for (T i = 0; i < sizeof(array); i++)
+	// 	{
+	// 		cout << i << endl;
+	// 	}
+		
+	for(T i : array){
+		cout << i << endl;
+	}
 }
 
 int main() {
-	int n;
-	
-	cin >> n;
-	vector<int> int_vector(n);
-	for (int i = 0; i < n; i++) {
-		int value;
-		cin >> value;
-		int_vector[i] = value;
-	}
-	
-	cin >> n;
-	vector<string> string_vector(n);
-	for (int i = 0; i < n; i++) {
-		string value;
-		cin >> value;
-		string_vector[i] = value;
-	}
+    int n;
+    
+    cin >> n;
+    vector<int> int_vector(n);
+    for (int i = 0; i < n; i++) {
+        int value;
+        cin >> value;
+        int_vector[i] = value;
+    }
+    
+    cin >> n;
+    vector<string> string_vector(n);
+    for (int i = 0; i < n; i++) {
+        string value;
+        cin >> value;
+        string_vector[i] = value;
+    }
 
-	printArray<int>(int_vector);
-	printArray<string>(string_vector);
+    printArray<int>(int_vector);
+    printArray<string>(string_vector);
 
-	return 0;
+    return 0;
 }
