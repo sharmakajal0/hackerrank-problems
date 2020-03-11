12e9e42f0ca2f72ebef8aa919bb412461b83e55e
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2020-03-11 22:20:53 +0530

    added cpp practice codes

---
+++ hackerrank/cpp_practice/basic-data-types/basic-data-types.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+
+int main() {
+    std::cout << "basic-data-types cpp program" << std::endl;
+    return 0;
+}
---
+++ hackerrank/cpp_practice/input-output/input-output.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a, b, c;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    cin >> a;
+    cin >> b;
+    cin >> c; 
+    cout << a + b + c << endl;
+    return 0;
+}
