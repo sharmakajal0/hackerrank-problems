63e72d1b44448b527eb8fb1c7109fcd2908ca1ec
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2020-03-16 23:09:40 +0530

    added abstract classes program in cpp

--- hackerrank/30-days-of-code/abstract-classes/abstract-classes.cpp
+++ hackerrank/30-days-of-code/abstract-classes/abstract-classes.cpp
@@ -5,59 +5,53 @@
 #include <algorithm>
 #include <string>
 using namespace std;
-class Book{
-    protected:
-        string title;
-        string author;
-    public:
-        Book(string t,string a){
-            title=t;
-            author=a;
-        }
-        virtual void display()=0;
-
+class Book {
+  protected:
+    string title;
+    string author;
+  public:
+    Book(string t,string a) {
+      title=t;
+      author=a;
+    }
+    virtual void display()=0;
 };
 
 // Write your MyBook class here
 class MyBook : public Book
 {
-    public:
-        int price;
-        MyBook(string title, string author, int price);
-    //   Class Constructor
-    //   
-    //   Parameters:
-    //   title - The book's title.
-    //   author - The book's author.
-    //   price - The book's price.
-    //
-    // Write your constructor here
-    MyBook :: MyBook(string title, string author, int price){
-        this -> title = title;
-        this -> author = author;
-        this -> price = price;
-    }
+  //  Class Constructor
+  //
+  //  Parameters:
+  //  title - The book's title.
+  //  author - The book's author.
+  //  price - The book's price.
+  //
+  // Write your constructor here
+  //  Function Name: display
+  //  Print the title, author, and price in the specified format.
+  //
+  // Write your method here
+  public:
+    int price;
+    MyBook(string title, string author, int price) : Book(title, author), price(price) {}
+
+  void display(){
+    cout << "Title: " << this -> title << endl;
+    cout << "Author: " << this -> author << endl;
+    cout << "Price: " << this -> price << endl;
+  }
 };
-    
-    //   Function Name: display
-    //   Print the title, author, and price in the specified format.
-    //
-    // Write your method here
-    void display(){
-        cout << "Title: " << title << endl;
-        cout << "Author: " << author << endl;
-        cout << "Price: " << price << endl;
-    }
     
 // End class
 
 int main() {
-    string title,author;
-    int price;
-    getline(cin,title);
-    getline(cin,author);
-    cin>>price;
-    MyBook novel(title,author,price);
-    novel.display();
-    return 0;
+  string title,author;
+  int price;
+  getline(cin,title);
+  getline(cin,author);
+  cin>>price;
+  MyBook novel(title,author,price);
+  novel.display();
+  return 0;
 }
